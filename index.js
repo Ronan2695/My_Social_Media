0f1943a58193375b578099206b300e896bb381e4
“Ronan2695” <rhtkmr2695@outlook.com>
2020-08-25 17:44:48 +0530

    View Engine setup

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8000;
 
 //use Express Router(Middleware)
 app.use('/', require('./routes/index'))
+//configuring our view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
 //Interpolation is being used here to print success and failure messages.
