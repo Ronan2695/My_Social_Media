9ecb06c341412f53c7d56a2ddf7a870c0705dbf4
“Ronan2695” <rhtkmr2695@outlook.com>
2020-08-25 16:06:38 +0530

    Added one more controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,21 @@
+//syntax for defining controller
+
+//module.exports.actionName = function(req,res){
+
+//}
+
+
+
+//controller action for homepage
+module.exports.home = function(req,res){
+
+    return res.end('<h1>Express is up for Rohit!</h1>')
+
+}
+
+//controller action for userspage
+module.exports.about = function(req,res){
+
+    return res.end('<h1>You are in the about page!</h1>')
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 //Express is required or called only once
 const express = require ('express');
 const router = express.Router();
+const homeController= require('../controllers/home_controller')
 
 console.log('router loaded')
 
-
-
+//accessing the controller
+router.get('/',homeController.home)
+router.get('/about',homeController.about)
 
 
 
