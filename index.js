f997b552f51ff21fe4b5e3d7955333b3b94a091b
“Ronan2695” <rhtkmr2695@outlook.com>
2020-08-25 15:10:53 +0530

    Setting up routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app= express();
 const port = 8000;
 
+//use Express Router(Middleware)
+app.use('/', require('./routes/index'))
+
 app.listen(port, function(err){
 //Interpolation is being used here to print success and failure messages.
     if(err)
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+//Express is required or called only once
+const express = require ('express');
+const router = express.Router();
+
+console.log('router loaded')
+
+
+
+
+
+
+
+
+//Exporting this, since it should be available to index.js
+module.exports = router;
