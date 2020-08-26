510a96255744e6d329fe780c63494e7b08273811
“Ronan2695” <rhtkmr2695@outlook.com>
2020-08-26 12:37:42 +0530

    Configured partials and layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,10 @@
 
 module.exports.profile=function(req,res){
 
-    return res.end('<h1> User Profile </h1>')
+    return res.render('user_profile',{
+
+        title:'profile'
+    });
 }
 
 module.exports.edit= function(req,res){
