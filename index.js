a8375775a4073d5c34de062c2c8921bf9314e019
“Ronan2695” <rhtkmr2695@outlook.com>
2020-08-26 15:27:57 +0530

    Configured the Database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app= express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose')
 
 //Setting up the static file
 app.use(express.static('./assets'));
