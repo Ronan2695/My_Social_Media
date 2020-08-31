484eb66bf7eb69068446f740569f2c37510da3d7
“Ronan2695” <rhtkmr2695@outlook.com>
2020-08-31 15:23:43 +0530

    Added authentication checks for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,15 @@
 //router page for posts
 const express = require('express');
 const router = express.Router();
+//for authentication purposes, we need to use passport
+const passport = require('passport');
 
 //accessing the controller of posts
 const postsController = require('../controllers/posts_controller') 
 
 //Route for creating posts. 
-router.post('/create', postsController.create)
+//We are adding passport middleware check, so that users who are signed in are only allowed to post.
+router.post('/create',passport.checkAuthentication, postsController.create)
 
 
 
