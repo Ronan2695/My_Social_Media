767e339bf48c2bc01397fbde8b4bbd34406dce10
“Ronan2695” <rhtkmr2695@outlook.com>
2020-09-01 05:52:37 +0530

    Added models for comments

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+
+    content:{
+        
+        type:String,
+        require:true
+    },
+
+    //comment belongs to an user (Referencing User Model)
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+
+    //Referencing Post model
+    post:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Post'
+    }
+
+
+},{
+    timestamps:true
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports=Comment;
