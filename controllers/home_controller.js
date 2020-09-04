bfea75915c031da72b7f1a6566acf90bfc905495
“Ronan2695” <rhtkmr2695@outlook.com>
2020-09-04 17:38:58 +0530

    Converting code into async and await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -18,27 +18,37 @@ const User = require('../models/users');
     // });
 
     //populating the user object using populate function
-module.exports.home = function(req,res){
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-
-        User.find({}, function(err,users){
-            return res.render('home',{
-                title:'Social_Media|Home',
-                posts:posts,
-                all_users:users
-
-             });
-      
+    //Finding a post the executing a query.
+    //First posts will get executed and then users will get executed.
+    //handling errors using catch
+module.exports.home = async function(req,res){
+    //error catching
+    try{
+        //populate the user of each post
+        let posts=await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        })
+        
+        let users=await User.find({});
+    
+        return res.render('home',{
+            title:'Social_Media|Home',
+            posts:posts,
+            all_users:users
+        
         });
-    })
+
+    }catch(err)
+    {
+        console.log('Error',err)
+        return
+    }
+       
 }
 
 //controller action for userspage
