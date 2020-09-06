f53791392ebf2a1912d93f284e47bdc16a6e2ad9
“Ronan2695” <rhtkmr2695@outlook.com>
2020-09-06 12:09:18 +0530

    Multer

--- models/users.js
+++ models/users.js
@@ -1,6 +1,11 @@
 //User Schema for login.
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+//Path to stored files
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
 const userSchema = new mongoose.Schema({
     
     email:{
@@ -15,11 +20,25 @@ const userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar:
+    {
+        type:String,
+
     }
 },{
     //This will give you the created at and updated at time.
     timestamps: true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
 
 
 //We are telling that this is a model in the database
