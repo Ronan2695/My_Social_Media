b1e2734ea2e5df51326c8c06e18a0d4c6835412c
“Ronan2695” <rhtkmr2695@outlook.com>
2020-09-07 22:24:35 +0530

    Deleting a post using API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,52 @@
-module.exports.index = function(req,res){
+const Post=require('../../../models/posts')
+const Comment = require('../../../models/comment');
+//displaying the list of posts from the DB in JSON format
+module.exports.index =async function(req,res){
+    
+
+    let posts=await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
 
    return res.json(200,{
        message:"List of Posts",
-       posts:[]
+       posts:posts
    })
+}
+
+//Deleting the POSTS using API, withoutb authentication
+
+module.exports.destroy= async function(req,res){
+    
+    let post = await Post.findById(req.params.id);
+
+    try{
+         //.id means converting the object id into string.
+        //Checking if current users are same
+        // console.log(req.params.id);
+        
+        // if(post.user == req.user.id){
+            post.remove();
+         await Comment.deleteMany({post:req.params.id});
+
+         return res.json(200,{
+             message:"Post and associated comments deleted successfully"
+         })
+        // }else
+        // {   
+        //     req.flash('error', 'Action cannot be performed')
+        //     return res.redirect('back')
+        // }
+    }catch(err)
+    { console.log('****',err)
+          return res.json(500,{
+            message:"Internal Server Error"
+        })
+     }
+    
 }
