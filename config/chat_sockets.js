bf26a5adea5e2c8ec820e6e4343c7940af076705
“Ronan2695” <rhtkmr2695@outlook.com>
2020-09-11 12:42:11 +0530

    Chatbox working

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,4 @@
+
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer);
 
@@ -6,6 +7,20 @@ module.exports.chatSockets = function(socketServer){
 
         socket.on('disconnect', function(){
             console.log('socket disconnected!');
+        });
+
+        
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+
+        // CHANGE :: detect send_message and broadcast to everyone in the room
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
         });
 
     });
